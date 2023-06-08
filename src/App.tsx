7431dd137cdc157c345f13fb7ceef2442a338bab
Caio Tracera <ctrcra@gmail.com>
2023-06-08 20:37:39 -0300

    style: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from './components/Header';
+
+import './global.css';
+
 export function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Header />
     </div>
   );
 }
