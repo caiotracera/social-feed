da43893cb52487f06569e402f2448689f0da646d
Caio Tracera <ctrcra@gmail.com>
2023-06-24 21:31:16 -0300

    feat(post): function to add new comment

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
+import { faker } from '@faker-js/faker';
+
 import { Comment } from '../Comment';
+import { CommentProps } from '../Comment/types';
 import { Avatar } from '../Avatar';
 
 import styles from './post.module.css';
 import { PostProps } from './types';
 
 export function Post({ author, comments, content, publishedAt }: PostProps) {
+  const [postComments, setPostComments] = useState<CommentProps[]>(comments);
+  const [newCommentText, setNewCommentText] = useState<string>('');
+
+  function handleCreateNewComment(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setPostComments((prevState) => [
+      ...prevState,
+      {
+        id: faker.string.uuid(),
+        content: newCommentText,
+        publishedAt: new Date().toISOString(),
+        author: author,
+        likes: 0,
+      },
+    ]);
+
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange(
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) {
+    setNewCommentText(event.target.value);
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -22,16 +52,21 @@ export function Post({ author, comments, content, publishedAt }: PostProps) {
         <p dangerouslySetInnerHTML={{ __html: content }} />
       </div>
 
-      <form className={styles.commentForm}>
+      <form className={styles.commentForm} onSubmit={handleCreateNewComment}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder={'Deixe um comentário'} />
+        <textarea
+          placeholder={'Deixe um comentário'}
+          name={'comment'}
+          onChange={handleNewCommentChange}
+          value={newCommentText}
+        />
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
 
       <div className="commentList">
-        {comments.map((comment) => (
+        {postComments.map((comment) => (
           <Comment key={comment.id} {...comment} />
         ))}
       </div>
